8ccbd70909d76beb5170d9a8367eb8667365c6d5
user <user@DESKTOP-8AALVP5>
2021-03-15 16:30:01 +0100

    hhh

--- EnsaAbscence/EnsaAbscence.Android/MainActivity.cs
+++ EnsaAbscence/EnsaAbscence.Android/MainActivity.cs
@@ -15,12 +15,11 @@ namespace EnsaAbscence.Droid
     {
 
        
-        protected override void OnCreate(Bundle savedInstanceState)
+        protected override void OnCreate(Bundle savedInstanceState)  
         {
+            base.OnCreate(savedInstanceState);
             TabLayoutResource = Resource.Layout.Tabbar;
             ToolbarResource = Resource.Layout.Toolbar;
-
-            base.OnCreate(savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
 
